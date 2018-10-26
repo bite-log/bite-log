75e69d66d741242246bc1728e5205523fcbc24e7
Hliana <liana.niehrah@gmail.com>
2018-10-26 14:18:20 -0700

    revised styling of gallery images rendered via js

--- js/app.js
+++ js/app.js
@@ -31,9 +31,6 @@ var BiteLogEntry = function(dish, restaurant, category, src, rating) {
   this.isfavorite = isfavorite;
   this.comment = comment;
 
-  this.width = 315;
-  this.height = 190;
-  
   biteLogEntryArray.push(this);
   restaurantArray.push(this.restaurant);
 };
@@ -42,7 +39,7 @@ console.log('this should be bitelot', BiteLogEntry);
 
 BiteLogEntry.prototype.renderSingleEntry = function (){
   var imgEl = document.createElement('img');
-  imgEl.setAttribute('class', 'food pic');
+  imgEl.setAttribute('class', 'food-pic');
   imgEl.src = this.src;
   displayEl.appendChild(imgEl);
 };
@@ -55,12 +52,12 @@ var renderAll = function () {
 
 //============Test Entries==================
 
-new BiteLogEntry('burger', 'Burger Joint', 'Pub Food', './assets/burger.jpg' , '5', 'yes', 'Delicious');
-new BiteLogEntry('pasta', 'Pasta Joint', 'Italian', './assets/pasta.png', '3', 'no', 'fantastic!');
-new BiteLogEntry('Pot Roast', 'Pot Roast City', 'Comfort Food', './assets/potroast.jpg', '4', 'no', 'Magical!');
-new BiteLogEntry('Chili', 'Chiliville', 'Comfort Food', './assets/chili.jpg', '2', 'no', 'good, not great');
-new BiteLogEntry('zoodles', 'Oodles O Noodles', 'Vegetarian', './assets/zoodles.jpg', '1','no', 'watery');
-new BiteLogEntry('cake', 'Just Cakes', 'Dessert', './assets/cake.jpg', '5', 'yes', 'so moist');
+new BiteLogEntry('burger', 'Burger Joint', 'Pub Food', './assets/burger.jpg' , '5', true, 'Delicious');
+new BiteLogEntry('pasta', 'Pasta Joint', 'Italian', './assets/pasta.png', '3', false, 'fantastic!');
+new BiteLogEntry('Pot Roast', 'Pot Roast City', 'Comfort Food', './assets/potroast.jpg', '4', false, 'Magical!');
+new BiteLogEntry('Chili', 'Chiliville', 'Comfort Food', './assets/chili.jpg', '2', false, 'good, not great');
+new BiteLogEntry('zoodles', 'Oodles O Noodles', 'Vegetarian', './assets/zoodles.jpg', '1', false, 'watery');
+new BiteLogEntry('cake', 'Just Cakes', 'Dessert', './assets/cake.jpg', '5', true, 'so moist');
 
 //==========Function Calls=================
 
