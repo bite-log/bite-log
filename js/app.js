a2b7066466b713aeb252a90f6df0109c1beff0ca
EvyHaan <Evy.Haan@gmail.com>
2018-10-27 22:58:37 -0700

    moved gallery object constructor code to gallery.js to fix bug

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ var restaurantArray = [];
 var biteLogEntryArray = [];
 var userProfileArray = [];
 
-var bitesCount = 0;
-// var uniqueRestaurauntCount = 0;
+
+
 
 var loggedIn = false;
 var currentUser;
@@ -30,88 +30,6 @@ var UserProfile = function (username, spice, src) {
   userProfileArray.push(this);
 };
 
-var BiteLogEntry = function(dish, restaurant, category, src, rating, isFav, comment) {
-  this.dishName = dish;
-  this.restaurant = restaurant;
-  this.category = category;
-  this.src = src;
-  this.rating = rating;
-
-  this.isFavorite = isFav;
-  this.comment = comment;
-
-  biteLogEntryArray.unshift(this);
-  restaurantArray.push(this.restaurant);
-  bitesCount++;
-  console.log(bitesCount);
-  this.renderBitesCount();
-};
-
-BiteLogEntry.prototype.renderBitesCount = function (){
-  var biteCountEl = document.getElementById('bite-count');
-  biteCountEl.textContent = bitesCount;
-};
-
-BiteLogEntry.prototype.renderSingleEntry = function (){
-  var imgEl = document.createElement('img');
-  imgEl.setAttribute('class', 'food-pic');
-  imgEl.src = this.src;
-  galleryView.appendChild(imgEl);
-};
-
-BiteLogEntry.prototype.renderSingleList = function(){
-  //Defining HTML elements
-  var figureEl = document.createElement('figure');
-  var figCapEl = document.createElement('figcaption');
-  var imgEl = document.createElement('img');
-  var favEl = document.createElement('i');
-  var h5El = document.createElement('h5');
-  var h6El = document.createElement('h6');
-  var spanEl = document.createElement('span');
-  var commentEl = document.createElement('p');
-
-  //Setting attributes
-  figureEl.setAttribute('class', 'list-container');
-  figCapEl.setAttribute('class', 'list-caption');
-  imgEl.setAttribute('class', 'food-pic-list');
-
-  //Defining
-  imgEl.src = this.src;
-  h5El.textContent = this.dishName;
-  h6El.textContent = this.restaurant;
-  spanEl.textContent = this.category;
-  commentEl.textContent = this.comment;
-
-  //Appending
-  listView.appendChild(figureEl);
-  figureEl.appendChild(imgEl);
-  figureEl.appendChild(figCapEl);
-  figCapEl.appendChild(h5El);
-
-  //if Favorited
-  if (this.isFavorite === true){
-    favEl.setAttribute('class', 'fas fa-heart');
-  } else{
-    favEl.setAttribute('class', 'far fa-heart');
-  }
-
-  //Number of stars
-  for(var i = 0; i < this.rating; i++){
-    var starEl = document.createElement('i');
-    starEl.setAttribute('class', 'fas fa-star');
-    figCapEl.appendChild(starEl);
-  }
-  for (var x = 0; x < (5 - this.rating); x++){
-    var starEl = document.createElement('i');
-    starEl.setAttribute('class', 'far fa-star');
-    figCapEl.appendChild(starEl);
-  }
-
-  figCapEl.appendChild(h6El);
-  figCapEl.appendChild(spanEl);
-  figCapEl.appendChild(favEl);
-  figCapEl.appendChild(commentEl);
-};
 
 //=============Event Handlers===============
 var createNewUserHandler = function(event){
@@ -170,7 +88,7 @@ var loginRedirect = function(event){
   } else if (loggedIn === false){
     var login = document.getElementById('login-form');
     var textEl = document.createElement('span');
-    textEl.setAttribute('class', 'login-required')
+    textEl.setAttribute('class', 'login-required');
     textEl.textContent = 'You must be logged in to continue.';
     login.insertBefore(textEl, login.children[1]);
   } 
@@ -183,14 +101,6 @@ var logout = function(event){
   window.location.href = 'index.html';
 };
 
-//============Test Entries==================
-
-new BiteLogEntry('burger', 'Burger Joint', 'Pub Food', './assets/burger.jpg' , '5', true, 'Delicious');
-new BiteLogEntry('pasta', 'Pasta Joint', 'Italian', './assets/pasta.png', 3, false, 'fantastic!');
-new BiteLogEntry('Pot Roast', 'Pot Roast City', 'Comfort Food', './assets/potroast.jpg', 4, false, 'Magical!');
-new BiteLogEntry('Chili', 'Chiliville', 'Comfort Food', './assets/chili.jpg', 2, false, 'good, not great');
-new BiteLogEntry('zoodles', 'Oodles O Noodles', 'Vegetarian', './assets/zoodles.jpg', 1, false, 'watery');
-new BiteLogEntry('cake', 'Just Cakes', 'Dessert', './assets/cake.jpg', 5, true, 'so moist');
 
 //===========Function Calls================
 
