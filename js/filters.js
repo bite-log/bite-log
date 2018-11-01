7cde27d7046a32af3e6175af99e51eeb3c8cbbff
EvyHaan <Evy.Haan@gmail.com>
2018-10-31 21:28:40 -0700

    filtering low to high and high to low working. working on filtering by favorites

--- js/filters.js
+++ js/filters.js
@@ -9,19 +9,20 @@
 // });
 
 // //======Sort from Lowest to Highest, Highest to Lowest=========
-var sortFromLowest = biteLogEntryArray.sort(function (a, b) {
-  return a.rating - b.rating;
-});
+// var sortFromLowest = biteLogEntryArray.sort(function (a, b) {
+//   return a.rating - b.rating;
+// });
 
-//====Filter Calls
-// favorites;
-sortFromLowest;
-sortFromLowest.reverse();
+// //====Filter Calls
+// // favorites;
+// sortFromLowest;
+// sortFromLowest.reverse();
 
 //=======Sort by Category=======
 var userCategory;
 
 var totalBites = [...biteLogEntryArray];
+
 var categorySortHandler = function(event){
   biteLogEntryArray = [];
   userCategory = categorySelections.options[categorySelections.selectedIndex].value;
@@ -35,6 +36,35 @@ var categorySortHandler = function(event){
       console.log(totalBites[i]);
     }
   }
+};
+
+// //=====Filter Handler====
+var userFilter;
+
+var filterHandler = function(event) {
+  userFilter = filterLogs.options[filterLogs.selectedIndex].value;
+  
+  var sortFromLowest = biteLogEntryArray.sort(function (a, b) {
+    return a.rating - b.rating;
+  });
+  
+  console.log('filter has changed to ' + userFilter);
+  if(userFilter === 'filter-favs') {
+    for(var i in totalBites) {
+      if(totalBites[i].isfavorite) {
+        biteLogEntryArray = [];
+        console.log('hereIamInTheIf!');
+        biteLogEntryArray.push(totalBites[i]);
+        console.log(totalBites[i]);
+      }
+    }
+  } else if (userFilter === 'filter-ratinghl') {
+    sortFromLowest.reverse();
+  } else if (userFilter === 'filter-ratinglw') {
+    sortFromLowest;
+  } else if (userFilter === 'default') {
+    biteLogEntryArray = [...totalBites];
+  }
   refreshSection();
   if (document.getElementById('grid-icon').className === 'icon-selected'){
     renderGallery(biteLogEntryArray);
@@ -43,39 +73,8 @@ var categorySortHandler = function(event){
   }
 };
 
-// //=====Filter Handler====
-var userFilter;
-
-var filterHandler = function(event) {
-  biteLogEntryArray = [];
-  userFilter = filterLogs.options[filterLogs.selectedIndex].value;
-  console.log('filter has changed to ' + userFilter);
-  if(event.target.id === 'favorites-filter') {
-    var favorites = biteLogEntryArray.filter(function (fav) {
-      return fav.isFavorite === true;
-    });
-    biteLogEntryArray.push(favorites);
-    refreshSection();
-    if (document.getElementById('grid-icon').className === 'icon-selected'){
-      renderGallery(biteLogEntryArray);
-    } else{
-      renderList(biteLogEntryArray);
-    }
-  } else if (event.target.id === 'highlow-filter') {
-    sortFromLowest.reverse();
-    refreshSection();
-    //render in order high to low
-  } else if (event.target.id === 'lowhigh-filter') {
-    sortFromLowest;
-    refreshSection();
-    //render in order low to high
-  } else { //if 'Filter By' remains
-    //do nothing
-  }
-};
-var filterLogs = document.getElementById('filter-by');
-filterLogs.addEventListener('onchange', filterHandler);
-
-
 var categorySelections = document.getElementById('food-category');
 categorySelections.addEventListener('onchange', categorySortHandler);
+
+var filterLogs = document.getElementById('filter-by');
+filterLogs.addEventListener('onchange', filterHandler);
