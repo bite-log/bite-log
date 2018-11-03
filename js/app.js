685a0eadcad16c8ad489384ab8295f7812d57581
Hliana <liana.niehrah@gmail.com>
2018-11-02 21:57:36 -0700

    fixed header. cleaned up code and made comments. adjusted filtering

--- js/app.js
+++ js/app.js
@@ -1,26 +1,20 @@
 'use-strict';
 
 //===========Global Variables==============
-var categoryArray = ['Chinese', 'Japanese', 'Vietnamese', 'Korean', 'Thai', 'Greek', 'Mexican', 'Mediterranean', 'Vegetarian', 'Comfort Food', 'Cocktail Bars', 'Food Truck', 'Italian', 'French', 'Fast Food', 'Fast Casual', 'Fine Dining', 'Pub', 'Coffee & Tea', 'Dessert', 'Barbecue', 'Buffet', 'Seafood', 'Deli', 'American', 'Pizza', 'Vegan', 'Tapas/Small Plates', 'Breakfast & Brunch'];
 var allRestaurantArray = [];
-var uniqueRestaurantArray = [];
 var biteLogEntryArray = [];
 var userProfileArray = [];
 var punArray = ['we\’re kind of a big dill', 'you look radishing', 'we\’re made pho each other', 'every day I’m trufflin\’', 'so we meat again', 'you guac my world', 'good chives only', 'penne for your thoughts?', 'lettuce turnip the beet', 'don\’t settle for being mediokra', 'keep calm and curry on', 'do you wanna taco \‘bout it?', 'walk this whey', 'you wanna pizza me?', 'jello from the other side', 'i donut know what i\’d do without you', 'olive you from my head tomatoes', 'two birds, one scone', 'it takes two to mango', 'absolutely spec-taco-lar!', 'you\'re flantastic!', 'you make miso happy', 'the pearsuit of happiness', 'don\'t go bacon my heart', 'another one bites the crust', 'it was mint to be!', 'party like it\'s sherbertday', 'love you a latte', 'hugs & quiches', 'hakuna frittata', 'pasta la vista, baby!'];
+var categoryArray = ['Chinese', 'Japanese', 'Vietnamese', 'Korean', 'Thai', 'Greek', 'Mexican', 'Mediterranean', 'Vegetarian', 'Comfort Food', 'Cocktail Bars', 'Food Truck', 'Italian', 'French', 'Fast Food', 'Fast Casual', 'Fine Dining', 'Pub', 'Coffee & Tea', 'Dessert', 'Barbecue', 'Buffet', 'Seafood', 'Deli', 'American', 'Pizza', 'Vegan', 'Tapas/Small Plates', 'Breakfast & Brunch'];
 
-var currentUser;
-var redirectLogin;
+var currentUser, redirectLogin;
 
 var galleryView = document.getElementById('gallery-view');
 var listView = document.getElementById('list-view');
-var newUserForm = document.getElementById('newuser-form');
 var loginForm = document.getElementById('login-form');
-var foodCategory = document.getElementById('food-category');
-var navigation = document.getElementById('nav');
 var logoutButton = document.getElementById('logout-button');
 
 //==========Constructor Functions==========
-
 var UserProfile = function (username, spice, src) {
   this.userName = username;
   this.spice = spice;
@@ -45,7 +39,7 @@ var createNewUserHandler = function(event){
   window.location.href = 'index.html';
 };
 
-var loginHandler = function(event){
+var loginHandler = function(event){ 
   event.preventDefault();
 
   var name = event.target.username.value;
@@ -53,7 +47,8 @@ var loginHandler = function(event){
 
   console.log(name + spice);
 
-  if (localStorage.getItem('users')){
+  //Checks if user input matches anything in local storage; if not, then it gives error message.
+  if (localStorage.getItem('users')){ 
     for (var i in userProfileArray){
       if (name === userProfileArray[i].userName && spice === userProfileArray[i].spice){
         currentUser = userProfileArray[i];
@@ -73,7 +68,7 @@ var loginHandler = function(event){
 var userDoesNotExist = function(){
   var errorMsg = document.getElementById('error-message');
   errorMsg.setAttribute('class', 'login-required')
-  errorMsg.textContent = 'User does not exist. Please create profile to continue.';
+  errorMsg.textContent = 'User does not exist and/or incorrect information entered. Please try again or create profile to continue.';
 };
 
 var userIsLoggedIn = function(){
@@ -96,10 +91,11 @@ var logout = function(event){
   window.location.href = 'index.html';
 };
   
-//==========Create category===============
+//==========Creates category drop-down based on array===============
 var categoryCreator = function(){
   categoryArray.sort();
   var dropDownList = document.getElementById('food-category');
+  dropDownList.removeAttribute('class', 'hidden');
   for (var i in categoryArray){
     var optionEl = document.createElement('option');
     optionEl.setAttribute('id', categoryArray[i]);
@@ -123,10 +119,11 @@ var grabUser = function(){
       currentUser = JSON.parse(localStorage.getItem('current-user'));
       logoutButton.addEventListener('click', logout);
     }  
-  } 
+  }
   if(!currentUser){
     redirectLogin = JSON.parse(localStorage.getItem('login-redirect'));
-    if (redirectLogin === true){
+    //works with login.js to redirect users if they try to go to gallery.html/add.html without being logged in
+    if (redirectLogin === true){ 
       var errorMsg = document.getElementById('error-message');
       errorMsg.setAttribute('class', 'login-required');
       errorMsg.textContent = 'You must be logged in to continue.';
@@ -137,11 +134,14 @@ var grabUser = function(){
 };
 
 var initialize = function(){
+  //Checks if there is a user logged in, then adds event listeners based on what form is currently onscreen.
   grabUser();
 
-  if (newUserForm){
+  if (document.getElementById('newuser-form')){
+    var newUserForm = document.getElementById('newuser-form');
     newUserForm.addEventListener('submit', createNewUserHandler);
   } else if (currentUser){
+    //If user is logged in, then a 'welcome back' prompt will appear in lieu of the login form.
     userIsLoggedIn();
     if (loginForm){
       var loginField = document.getElementById('login-field');
@@ -153,13 +153,12 @@ var initialize = function(){
       loginField.appendChild(h5El);
     }
   }
-
   if (!galleryView || !listView){
     randomPun();
   }
   if (loginForm){
     loginForm.addEventListener('submit', loginHandler);
-  } else if (foodCategory){
+  } else if (document.getElementById('add-log')){
     categoryCreator();
   }
 };
